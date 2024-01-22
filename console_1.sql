637e2af9fc36054e299742b75d42ea034dfac410
Anna Danchenko <53533713+anyaachan@users.noreply.github.com>
2024-01-22 23:31:31 +0100

    replace aggregate to find

--- console_1.sql
+++ console_1.sql
@@ -1,29 +1,7 @@
 // 1. Select all the employees that work as zoo keepers. Display their animal specialization and information.
-db.getSiblingDB("annadanc").getCollection("employee").aggregate([
-  {
-    $match: {"role.role_name": {$eq: "zoo_keeper"}}
-  },
-  {
-    $project: {"employee_id": 1, "animals_specialization": "$role.animals_specialization", "name": 1, "surname": 1, "phone": 1, "email": 1, "_id": 0}
-  },
-  {
-    $group: {
-      _id: null,
-      "distinct": {$addToSet: "$$ROOT"}
-    }
-  },
-  {
-    $unwind: {
-      path: "$distinct",
-      preserveNullAndEmptyArrays: false
-    }
-  },
-  {
-    $replaceRoot: {
-      newRoot: "$distinct"
-    }
-  }
-])
+db.employee.find({"role.role_name": "zoo_keeper"},
+                 {employee_id: 1, "animals_specialization": "$role.animals_specialization", name: 1, surname: 1, phone: 1, email: 1, _id: 0})
+
 
 //2. Select all animals that were not fed on December 2, 2023, by checking the absence of a corresponding feeding record on that date.
 db.animal.aggregate([
@@ -115,6 +93,7 @@ db.employee.aggregate([
     }
 ])
 
+
 // 5. Select all of the employees and list their role and specialization.
 db.employee.aggregate([
     {
@@ -154,7 +133,7 @@ db.section.aggregate([
         }
     },
     {
-        $match: {"animals_in_enc": { $size: 0 }}
+        $match: {"animals_in_enc": {$size: 0}}
     },
     {
         $sort: {enclosure_id: 1}
@@ -162,9 +141,193 @@ db.section.aggregate([
 ])
 
 // 7.  Determine the occupancy percentage of each enclosure, comparing the number of animals present to its total capacity, and orders the results from highest to lowest percentage of utilization.
-
+// The query produces the same results as SQL query.
+db.section.aggregate([
+    {
+    $unwind: "$enclosures"
+    },
+    {
+        $lookup: {
+            from: "animal",
+            localField: "enclosures.enclosure_id",
+            foreignField: "enclosure",
+            as: "animals_in_enc"
+        }
+    },
+    {
+        $project: {
+            enclosure_id: "$enclosures.enclosure_id",
+            animal_count: { $size: "$animals_in_enc" },
+            capacity: "$enclosures.capacity",
+            occupancy_percentage: {
+                $round: [
+                    {$multiply: [
+                        {$divide: [{$size: "$animals_in_enc"}, "$enclosures.capacity"]},
+                        100
+                    ]
+                }, 0
+                ]
+            }
+        }
+    },
+    {
+        $sort: {occupancy_percentage: -1}
+    }
+])
 
 // 8. Select all Mammals that were fed at least once.
-
+// The query returns the same results as the SQL query.
+db.animal.aggregate([
+    {$unwind: "$feedings"},
+    {
+        $lookup: {
+            from: "employee",
+            localField: "feedings.employee_id",
+            foreignField: "employee_id",
+            as: "employee_info"
+        }
+    },
+    {$match: {"employee_info.role.animals_specialization": "Mammals"}},
+    {
+        $group: {
+            _id: "$animal_id",
+            name: {$first: "$name"},
+            enclosure: {$first: "$enclosure"},
+            species_binominal_name: {$first: "$species_binominal_name"},
+        }
+    }
+])
 
 // 9. Select all of the enclosures in the zoo which were guarded by security guards with Advanced security level.
+db.employee.aggregate([
+
+])
+
+// 10. Count how many times each employee had a shift in each section.
+// The results of the query are not particularly same as in the SQL database, as the query does not display the shift count
+// for the section if the count is 0.
+db.section.aggregate([
+    {$unwind: "$security_shifts"},
+    {$group: {
+        _id: {employee_id: "$security_shifts.employee_id",
+            section_name: "$section_name"},
+        shifts_count: {$sum: 1}
+    }},
+    {
+        $project: {
+            _id: 0, // excluding the _id field
+            employee_id: "$_id.employee_id",
+            section_name: "$_id.section_name",
+            shifts_count: 1
+        }
+    },
+    {
+        $sort: {"employee_id": 1}
+    }
+])
+
+// 11. Select all of the zoo keepers whos feedings are sponsored by godparents
+// The query is generated automatically using the following transformed SQL query:
+    //SELECT DISTINCT e.employee_id, e.name, e.surname
+    //FROM godparent g
+    //INNER JOIN animal ON g.sponsored_animal.animal_id = animal.animal_id
+    //INNER JOIN employee e ON e.employee_id = animal.feedings.employee_id;
+
+db.getSiblingDB("annadanc").getCollection("godparent").aggregate([
+  {
+    $project: {"g": "$$ROOT", "_id": 0}
+  },
+  {
+    $lookup: {
+      localField: "g.sponsored_animal.animal_id",
+      from: "animal",
+      foreignField: "animal_id",
+      as: "animal"
+    }
+  },
+  {
+    $unwind: {
+      path: "$animal",
+      preserveNullAndEmptyArrays: false
+    }
+  },
+  {
+    $lookup: {
+      localField: "animal.feedings.employee_id",
+      from: "employee",
+      foreignField: "employee_id",
+      as: "e"
+    }
+  },
+  {
+    $unwind: {
+      path: "$e",
+      preserveNullAndEmptyArrays: false
+    }
+  },
+  {
+    $project: {"employee_id": "$e.employee_id", "name": "$e.name", "surname": "$e.surname", "_id": 0}
+  },
+  {
+    $group: {
+      _id: null,
+      "distinct": {$addToSet: "$$ROOT"}
+    }
+  },
+  {
+    $unwind: {
+      path: "$distinct",
+      preserveNullAndEmptyArrays: false
+    }
+  },
+  {
+    $replaceRoot: {
+      newRoot: "$distinct"
+    }
+  }
+])
+
+
+// 12. Select all of the animals in the zoo that both have animal parents in the zoo and godparents (sponsors).
+// This query provides the same output as in the SQL database, however, it was decided to add a field with an array of godparents sponsoring
+db.animal.aggregate([
+    {$lookup: { // Join animal and godparent tables
+        from: "godparent",
+        localField: "animal_id",
+        foreignField: "sponsored_animal.animal_id",
+        as: "godparent_animal_match"
+    }},
+    {$match: {
+            "parents.animal_parent_id": {$ne: null, $not: {$size: 0}},
+            "godparent_animal_match": {$ne: null, $not: {$size: 0}}
+        }
+    },
+    {$unwind: "$godparent_animal_match"},
+    {
+    $group: {
+            _id: "$animal_id", // grouping key
+            birth_date: {$first: "$birth_date"},
+            name: {$first: "$name"},
+            sex: {$first: "sex"},
+            enclosure: {$first: "enclosure"},
+            species_binominal_name: {$first: "$species_binominal_name"},
+            sponsoring_godparents: {$push: "$godparent_animal_match.godparent_id"}}
+    },
+    {
+        $project: {
+            animal_id: 1,
+            birth_date: 1,
+            species_binominal_name: 1,
+            name: 1,
+            sex: 1,
+            enclosure: 1,
+            sponsoring_godparents: 1
+        }
+    }
+])
+
+// 13. Select animals that were only fed by Olivia and not by any other zoo keeper.
+
+// 14. Select IDs of the security guards that had a shift in each section.
+
+// 15. Calculate the number of different species in each section, ordering sections by the count of unique species from highest to lowest.
